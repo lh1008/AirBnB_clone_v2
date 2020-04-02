df9f171b05ca5259965a94e36497bf46a9fe93e2
lh008 <paintluis@gmail.com>
2020-04-02 17:45:17 -0500

    Missing Base in import user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 
 class User(BaseModel, Base):
@@ -12,7 +12,7 @@ class User(BaseModel, Base):
         last_name: last name
     """
     __tablename__ = 'users'
-    
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
