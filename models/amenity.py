3c1066f8064cb15c2acb4d73adb283daaab81e9a
noah172 <jsebastian.garcia1995@gmail.com>
2020-04-03 04:14:46 +0000

    rollbac

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from models.place import place_amenity
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
-    __tablename__ = 'amenities'
-
-    name = Column(String(128), nullable=False)
-
-    # relations
-    place_amenities = relationship('Place', secondary=place_amenity)
+    name = ""
