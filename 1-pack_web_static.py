0c2f37cfc497b1dfb9805b63efb84860e40b1e54
lh008 <paintluis@gmail.com>
2020-04-13 21:20:26 -0500

    Compress before sending, Task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Fabric module script that generates a .tgz archive """
+from fabric.api import local
+from os import path, mkdir
+from datetime import datetime
+
+t = datetime.now()
+
+def do_pack():
+    """ Method to generate the .tgz file """
+    if path.exists('./versions'):
+        return None
+    else:
+        mkdir('versions')
+        time = 'versions/web_static_{}{}{}{}{}{}.tgz'\
+            .format(t.year, t.month, t.day, t.hour, t.minute, t.second)
+        local("tar -cvzf " + time + " ./web_static/")
+        if local.succeeded:
+            return t
