60920f3099efbe57dbb498329bd49213eb1a7815
lh008 <paintluis@gmail.com>
2020-04-21 18:15:36 -0500

    Added if __name__ app.run to 0.0.0.0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """ Module script that startsa Flask web application """
 from flask import Flask
 app = Flask(__name__)
 
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def main():
+    """ Method that returns displayed message """
     return 'Hello HBNB!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
