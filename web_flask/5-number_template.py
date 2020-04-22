44e3583bdaa44a0e921cca4657defb912f9f5d8d
lh008 <paintluis@gmail.com>
2020-04-22 13:34:20 -0500

    Number template, Task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Module script that startsa Flask web application """
+from flask import Flask, escape, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def main():
+    """ Method that returns displayed message """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """ Method routing to hbnb decorator """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """ Method that displays C followed by value """
+    return 'C %s' % escape(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': None})
+@app.route('/python/<text>')
+def py_text(text):
+    """ Method that displays Python followed by value """
+    if text is None:
+        return 'Python %s' % escape("is cool")
+    elif text:
+        return 'Python %s' % escape(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def n_int(n):
+    """ Method that displays n is an int """
+    return '%d is a number' % n
+
+
+@app.route('/number_template/')
+@app.route('/number_template/<int:n>')
+def tem_plate(n=None):
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
